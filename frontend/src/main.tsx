4557d819fa9e80067de154cbb43ac1287a6995a7
Ivan Dryha <driga.ya27@gmail.com>
2025-04-19 15:25:03 +0300

    init chakra-ui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "@/components/ui/provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider>
+      <App />
+    </Provider>
+  </StrictMode>
+);
