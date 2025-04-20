92a09ccf20a1aecf0c480302080087a23eb4ce2e
Ivan Dryha <driga.ya27@gmail.com>
2025-04-20 23:01:59 +0300

    feat: link to home page in sidebar

--- frontend/src/components/LastSummariesSidebar/LastSummariesSidebar.tsx
+++ frontend/src/components/LastSummariesSidebar/LastSummariesSidebar.tsx
@@ -1,14 +1,32 @@
 import { useSummaries } from "@/api/hooks/summaries.hook";
-import { Box, For, Text } from "@chakra-ui/react";
+import { Box, For, HStack, IconButton, Text } from "@chakra-ui/react";
+import { MdOutlineCreateNewFolder } from "react-icons/md";
+import { useNavigate } from "react-router";
 
 export function LastSummariesSidebar() {
   const { summaries } = useSummaries(10);
 
+  const navigate = useNavigate();
+
+  const navigateToHomePage = () => {
+    navigate("/");
+  };
+
+  const navigateToSummaryPage = (id: number) => {
+    navigate(`/summary/${id}`);
+  };
+
   return (
     <Box bg="gray.900" w="28rem" h="full" p="4">
-      <Text as="h3" fontWeight="semibold" mb="4">
-        Last summaries
-      </Text>
+      <HStack mb="4" justifyContent="space-between">
+        <Text as="h3" fontWeight="semibold">
+          Last summaries
+        </Text>
+
+        <IconButton variant="outline" onClick={navigateToHomePage}>
+          <MdOutlineCreateNewFolder />
+        </IconButton>
+      </HStack>
 
       {summaries?.length === 0 ? (
         <Text fontStyle="italic">No summaries</Text>
@@ -23,6 +41,7 @@ export function LastSummariesSidebar() {
               borderRadius="md"
               _hover={{ bg: "gray.700" }}
               cursor="pointer"
+              onClick={() => navigateToSummaryPage(item.id)}
             >
               {item.title}
             </Box>
