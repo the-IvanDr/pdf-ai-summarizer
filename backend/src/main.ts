3d481c5535ebd415a5df3c77ff34251a105f8870
Ivan Dryha <driga.ya27@gmail.com>
2025-04-21 22:20:59 +0300

    style: extract config values to environment variables

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_APP_URL ?? 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
 
--- frontend/src/api/configs/constants.ts
+++ frontend/src/api/configs/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:3000";
+export const API_URL = import.meta.env.VITE_API_URL;
 export const FILES_URL = `${API_URL}/public/files`;
